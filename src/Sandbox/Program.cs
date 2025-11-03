c5f95263e98479c175bd3cf85dea0953eea9506a
Patrik Svensson <patrik@patriksvensson.se>
2025-11-03 02:51:02 +0100

    Make it possible to quit the sandbox

--- src/Sandbox/Program.cs
+++ src/Sandbox/Program.cs
@@ -6,13 +6,20 @@ public static class Program
 {
     public static void Main(string[] args)
     {
+        var running = true;
+
         using var terminal = new Terminal();
         var renderer = new Renderer(terminal);
 
-        while (true)
+        while (running)
         {
-            renderer.Draw((frame, elapsed) =>
+            renderer.Draw((_, _) =>
             {
+                // Time to quit?
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Q)
+                {
+                    running = false;
+                }
             });
         }
     }
